09d882cec246d6d132603d57ac61bd087d24833e
ZaL <z4ls@live.com>
2019-05-10 05:06:10 +0400

    add pods

---
+++ ios/Pods/Target Support Files/Pods-atspot/Pods-atspot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_atspot : NSObject
+@end
+@implementation PodsDummy_Pods_atspot
+@end
--- ios/Pods/Target Support Files/Pods-spotwash/Pods-spotwash-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_spotwash : NSObject
-@end
-@implementation PodsDummy_Pods_spotwash
-@end
